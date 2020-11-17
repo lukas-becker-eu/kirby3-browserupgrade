a208844f35675eb7da34fdb4a11d94c64a2c3ee0
Lukas Becker <lukas@candyblue.agency>
2020-11-17 21:20:37 +0100

    Update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Kirby::plugin('candyblue/kirby3-browserupgrade', [
       'candyblue.kirby3-browserupgrade.upgrade-your-browser' => 'You are using an&nbsp;<strong>outdated browser</strong>. It has known security flaws and may not display all features of this and other websites. Please&nbsp;<a href="http://outdatedbrowser.com/" target="_blank">upgrade your browser</a>&nbsp;to improve your experience.'
     ],
     'de' => [
-      'candyblue.kirby3-browserupgrade.enable-js' => 'Bitte aktiviere Javascript, damit diese Website korrekt funktioniert.',
+      'candyblue.kirby3-browserupgrade.enable-js' => 'Bitten aktivieren Sie Javascript, damit diese Website korrekt funktioniert.',
       'candyblue.kirby3-browserupgrade.upgrade-your-browser' => 'Sie benutzen einen&nbsp;<strong>veralteten Browser</strong> mit Sicherheitsschwachstellen und können vermutlich nicht alle Funktionen dieser und anderer Webseiten nutzen. Bitte&nbsp;<a href="http://outdatedbrowser.com/de" target="_blank">aktualisieren Sie ihren Browser</a>, und verbessern Sie ihr Nutzererlebnis.'
     ]
   ],
